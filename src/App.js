cb1b15c8a3e0f9cae30570537beae87682d741ae
Vincentyr <vincentsiowyongren@gmail.com>
2021-10-05 17:04:07 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
 import React from 'react';
 import { Route, Switch, Link } from "react-router-dom";
-import About from './components/About';
-import Navbar from './components/Navbar';
-import Contact from './components/Contact';
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+// import About from './components/About';
+// import Navbar from './components/Navbar';
+// import Contact from './components/Contact';
+// import Projects from "./components/Projects";
+// import Skills from "./components/Skills";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello Wor</h1>
-    </div>
-    // <main>
-    //   <Navbar />
-    //   <About />
-    //   <Projects />
-    //   <Skills />
-    //   <Contact />
-    // </main>
-  );
+ class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Wor</h1>
+      </div>
+      // <main>
+      //   <Navbar />
+      //   <About />
+      //   <Projects />
+      //   <Skills />
+      //   <Contact />
+      // </main>
+    );
+  }
 }
 
+export default App;
