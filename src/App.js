e4ec2be35477b78e9867bf5c7d363dcbd0522d90
Vincentyr <vincentsiowyongren@gmail.com>
2021-10-05 16:52:01 +0800

    about

--- src/App.js
+++ src/App.js
@@ -7,19 +7,15 @@ import Contact from './components/Contact';
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
-class App extends React.Component {
-  render() {
-      return (
-        <main>
-          <Navbar />
-          <About />
-          <Projects />
-          <Skills />
-          <Contact />
-        </main>
-    );
-  }
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  );
 }
-
-export default App;
 
